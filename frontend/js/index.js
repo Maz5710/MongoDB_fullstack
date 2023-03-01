014f7ee140b46e79f8a1ff3c7ffeedf7dff78ff2
Maz5710 <marian.arnot@gmail.com>
2023-03-01 16:28:14 +1300

    Updated images linked to Google drive folder

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,8 +1,8 @@
-/*jshint esversion: 6 */ 
+/*jshint esversion: 6 */
 
 console.log('frontend script is working');
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     let url;
 
@@ -11,91 +11,113 @@ $(document).ready(function(){
         url: 'config.json',
         type: 'GET',
         dataType: 'json',
-        success: function(configData){
+        success: function (configData) {
             console.log(configData.SERVER_URL, configData.SERVER_PORT);
             url = `${configData.SERVER_URL}:${configData.SERVER_PORT}`;
             console.log(url);
+            getAllProducts();
         },
-        error: function(error){
+        error: function (error) {
             console.log(error);
         }
     });
 
     function getAllProducts() {
-        $.ajax({
-            url: `http://${url}/allProductsFromDB`,
-            type: 'GET',
-            dataType: 'json',
-            success: function(productsFromMongo) {
-                document.getElementById('result').innerHTML = '';
-                for(let i = 0; i < productsFromMongo.length; i++ ){
-                    console.log(productsFromMongo[i]);
-                    document.getElementById('result').innerHTML += `
-                    <div class="col-4 mt-3 mb-3">
-                        <div class="card">
-                        <img class="card-img-top" src="${productsFromMongo[i].image_url}" alt="${productsFromMongo[i].name} image">
-                            <div class="card-body">
-                                <h5 class="card-title">${productsFromMongo[i].name}</h5>
-                                 <p class="card-text">${productsFromMongo[i].price}</p>
-                                 <button value="${productsFromMongo[i]._id}" class="btn delete btn-primary" type="button" name="button">Delete</button>
-                                 <button value="${productsFromMongo[i]._id}" data-bs-toggle="modal" data-bs-target="#editModal" class="btn edit btn-primary" type="button" name="button">Edit</button>
-                                 <button value="${productsFromMongo[i]._id}" data-bs-toggle="modal" data-bs-target="#readmoreModal" class="btn readmore btn-primary" type="button" name="button">Read More</button>
+        let userid = sessionStorage.getItem('userID');
+        if (!userid) {
+            alert('Please log in.');
+        } else {
+            $.ajax({
+                url: `http://${url}/allProductsFromDB`,
+                type: 'GET',
+                dataType: 'json',
+                success: function (productsFromMongo) {
+                    let results = document.getElementById('result');
+                    results.innerHTML = '';
+                    for (let i = 0; i < productsFromMongo.length; i++) {
+                        let createdBy = productsFromMongo[i].user_id;
+                        console.log(productsFromMongo[i]);
+                        if (createdBy === userid) {
+                            results.innerHTML += `
+                            <div class="col-4 mt-3 mb-3">
+                                <div class="card">
+                                <img class="card-img-top" src="${productsFromMongo[i].image_url}" alt="${productsFromMongo[i].name} image">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${productsFromMongo[i].name}</h5>
+                                         <p class="card-text">${productsFromMongo[i].price}</p>
+                                         <button value="${productsFromMongo[i]._id}" class="btn delete btn-primary" type="button" name="button">Delete</button>
+                                         <button value="${productsFromMongo[i]._id}" data-bs-toggle="modal" data-bs-target="#editModal" class="btn edit btn-primary" type="button" name="button">Edit</button>
+                                         <button value="${productsFromMongo[i]._id}" data-bs-toggle="modal" data-bs-target="#readmoreModal" class="btn readmore btn-primary" type="button" name="button">Read More</button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    `;
-                    editProducts();
-                    deleteButtons();
-                    readmore();
+                            `;
+                        } else {
+                            results.innerHTML += `
+                            <div class="col-4 mt-3 mb-3">
+                                <div class="card">
+                                <img class="card-img-top" src="${productsFromMongo[i].image_url}" alt="${productsFromMongo[i].name} image">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${productsFromMongo[i].name}</h5>
+                                         <p class="card-text">${productsFromMongo[i].price}</p>
+                                         <button value="${productsFromMongo[i]._id}" data-bs-toggle="modal" data-bs-target="#readmoreModal" class="btn readmore btn-primary" type="button" name="button">Read More</button>
+                                    </div>
+                                </div>
+                            </div>
+                            `;
+                        }
+
+                        editProducts();
+                        deleteButtons();
+                        readmore();
+                    }
+                },
+                error: function () {
+                    alert('unable to get products');
                 }
-            },
-            error: function() {
-                alert('unable to get products');
-            }
-        });
+            });
+        }
     }
 
-    // // Get all products
-    // getAllProducts();
 
     //View Products onclick of View Products Button
-    $('#viewProducts').click(function(){
-       getAllProducts();
-    });// End of View Products
+    $('#viewProducts').click(function () {
+        getAllProducts();
+    }); // End of View Products
 
 
     //add a product form a click
-    $('#addProduct').click(function(event){
+    $('#addProduct').click(function (event) {
         event.preventDefault();
         let name = $('#a-name').val();
         let price = $('#a-price').val();
         let image_url = $('#a-imageurl').val();
         let userid = sessionStorage.getItem('userID');
         console.log(userid);
-        console.log(name,price, image_url);
-        if (name == '' || price == '' || image_url == '' || !userid){
-        alert('Please login and enter all details');
+        console.log(name, price, image_url);
+        if (name == '' || price == '' || image_url == '' || !userid) {
+            alert('Please login and enter all details');
         } else {
-        $.ajax({
-            url : `http://${url}/addProduct`,
-            type : 'POST',
-            data :{
-                name: name,
-                price: price,
-                image_url:image_url,
-                user_id: userid
-            },
-            success : function(product){
-            console.log(product);
-            alert ('product added');
-            getAllProducts();
-            },
-            error : function(){
-            console.log('error: cannot call api');
-            }// End of error
-          });// End of ajax
-        }// End of else
-    });// End of add Product Click
+            $.ajax({
+                url: `http://${url}/addProduct`,
+                type: 'POST',
+                data: {
+                    name: name,
+                    price: price,
+                    image_url: image_url,
+                    user_id: userid
+                },
+                success: function (product) {
+                    console.log(product);
+                    alert('product added');
+                    getAllProducts();
+                },
+                error: function () {
+                    console.log('error: cannot call api');
+                } // End of error
+            }); // End of ajax
+        } // End of else
+    }); // End of add Product Click
 
 
     // Giving our "Save Changes" button an id for each product
@@ -112,7 +134,7 @@ $(document).ready(function(){
 
 
     // UPDATE PRODUCT from Modal Save Button
-    $('.saveChange').click(function(event){
+    $('.saveChange').click(function (event) {
         event.preventDefault();
         let productId = this.value;
         let productName = $('#productName').val();
@@ -131,16 +153,16 @@ $(document).ready(function(){
                     price: productPrice,
                     image_url: productImageUrl
                 },
-                success: function(data){
+                success: function (data) {
                     console.log(data);
                     getAllProducts();
                 },
-                error: function(){
+                error: function () {
                     console.log('error: cannot update post');
-                }// End of error
+                } // End of error
             }); // End of ajax
-        }// End of if
-    });// End of update click
+        } // End of if
+    }); // End of update click
 
     // DELETE PRODUCT
     function deleteButtons() {
@@ -158,16 +180,16 @@ $(document).ready(function(){
                     $.ajax({
                         url: `http://${url}/deleteProduct/${productId}`,
                         type: 'DELETE',
-                        success: function() {
+                        success: function () {
                             console.log('deleted');
                             alert('Product Deleted');
                             getAllProducts();
                         },
-                        error: function() {
+                        error: function () {
                             console.log('error: cannot delete due to call on api');
-                        }// error
+                        } // error
                     }); // ajax
-                }// if
+                } // if
             });
         });
     }
@@ -185,23 +207,22 @@ $(document).ready(function(){
                     type: `GET`,
                     dataType: `json`,
                     success: function (product) {
-                        console.log(product);   
-                        let readmoreBody = document.getElementById('readMoreBody');      
+                        console.log(product);
+                        let readmoreBody = document.getElementById('readMoreBody');
                         readmoreBody.innerHTML = `
                         <div class="row featurette">
                         <div class="col-md-7">
                             <h2 class="featurette-heading fw-normal lh-1">${product.name} <span
                                     class="text-muted">$${product.price}</span></h2>
-                            <p class="lead">Some great placeholder content for the first featurette here. Imagine some
-                                exciting prose here.</p>                                
+                            <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>                                
                         </div>
                         <div class="col-md-5 ">
                             <img src="${product.image_url}" class="w-100" alt="${product.name}">
                         </div>
                     </div>
-                        `;                
+                        `;
                     },
-                    error: function() {
+                    error: function () {
                         alert('Unable to find product');
                     }
 
@@ -217,9 +238,9 @@ $(document).ready(function(){
     // Register User
     $('#r-submit').click(function (event) {
         event.preventDefault();
-        let username =$('#r-username').val();
-        let email =$('#r-email').val();
-        let password =$('#r-password').val();
+        let username = $('#r-username').val();
+        let email = $('#r-email').val();
+        let password = $('#r-password').val();
         console.log(username, email, password);
 
         if (username == '' || email == '' || password == '') {
@@ -233,9 +254,9 @@ $(document).ready(function(){
                     email: email,
                     password: password
                 },
-                success: function(user) {
+                success: function (user) {
                     console.log(user); // remove when dev is finished
-                    if (user !== 'username already taken'){
+                    if (user !== 'username already taken') {
                         sessionStorage.setItem('userID', user['_id']);
                         sessionStorage.setItem('userName', user['username']);
                         sessionStorage.setItem('userEmail', user['email']);
@@ -248,64 +269,65 @@ $(document).ready(function(){
                         $('#r-password').val('');
                     } // else
                 },
-                error: function() {
+                error: function () {
                     console.log('error: cannot call add user api');
-                }// error
+                } // error
             }); // end of ajax
         } // end of else
     }); // end of submit user click
 
 
     //Login User
-  $('#login-submit').click(function(event) {
-    event.preventDefault();
-    let username = $('#login-username').val();
-    let password = $('#login-password').val();
+    $('#login-submit').click(function (event) {
+        event.preventDefault();
+        let username = $('#login-username').val();
+        let password = $('#login-password').val();
 
-    console.log(username, password);
+        console.log(username, password);
 
-    if (username == '' || password == '') {
-      alert('Please enter all details');
-    } else {
-      $.ajax({
-        url: `http://${url}/loginUser`,
-        type: 'POST',
-        data: {
-          username: username,
-          password: password
-        },
-        success: function(user) {
-          console.log(user);
+        if (username == '' || password == '') {
+            alert('Please enter all details');
+        } else {
+            $.ajax({
+                url: `http://${url}/loginUser`,
+                type: 'POST',
+                data: {
+                    username: username,
+                    password: password
+                },
+                success: function (user) {
+                    console.log(user);
 
-          if (user == 'User not found. Please register') {
-            alert('User not found. Please Register');
-          } else if (user == 'not authorized') {
-            alert('Please try with correct details');
-            $('#login-username').val('');
-            $('#password').val('');
-          } else {
-            sessionStorage.setItem('userID', user['_id']);
-            sessionStorage.setItem('userName', user['username']);
-            sessionStorage.setItem('userEmail', user['email']);
-            console.log(sessionStorage);
-            let loggedIn = document.querySelector('.logged-in');
-            loggedIn.innerHTML = `<p>Logged in as <span class="text-danger">${username.toUpperCase()}</span></p>`;
-            alert(`Welcome back ${username.toUpperCase()}!`);
-          } // end of ifs
-        }, //success
-        error: function() {
-          console.log('error: cannot call api');
-          alert('Unable to login - unable to call api');
-        } //error
-      }); //end of ajax
-    } //end of else
-  }); //end of login click function
+                    if (user == 'User not found. Please register') {
+                        alert('User not found. Please Register');
+                    } else if (user == 'not authorized') {
+                        alert('Please try with correct details');
+                        $('#login-username').val('');
+                        $('#password').val('');
+                    } else {
+                        sessionStorage.setItem('userID', user['_id']);
+                        sessionStorage.setItem('userName', user['username']);
+                        sessionStorage.setItem('userEmail', user['email']);
+                        console.log(sessionStorage);
+                        let loggedIn = document.querySelector('.logged-in');
+                        loggedIn.innerHTML = `<p>Logged in as <span class="text-danger">${username.toUpperCase()}</span></p>`;
+                        alert(`Welcome back ${username.toUpperCase()}!`);
+                    } // end of ifs
+                }, //success
+                error: function () {
+                    console.log('error: cannot call api');
+                    alert('Unable to login - unable to call api');
+                } //error
+            }); //end of ajax
+        } //end of else
+    }); //end of login click function
 
     // Logout
-    $('#logout').click(function(){
+    $('#logout').click(function () {
         sessionStorage.clear();
         alert('You are now logged out');
         console.log(sessionStorage);
+        window.location.href = '/about.html';
     });
 
 
